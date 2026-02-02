3c8a01d939d00e615432584787ad469286431d40
danishzayan <danishzayan6@gmail.com>
2026-02-02 16:00:19 +0530

    when in the production then use this when in the developement then use this of the localhost

--- client/next.config.js
+++ client/next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
+    const isProd = process.env.NODE_ENV === 'production';
     return [
       {
         source: '/api/v1/:path*',
-        destination: 'http://localhost:8000/api/v1/:path*',
+        destination: isProd
+          ? 'https://mini-los.onrender.com/api/v1/:path*'
+          : 'http://localhost:8000/api/v1/:path*',
       },
-    ]
+    ];
   },
 }
 
